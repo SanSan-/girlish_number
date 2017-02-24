27896523a98def0b2d8020f677a4d05fc11646cf
alexfranz <ut1339@gmail.com>
2017-02-25 00:55:58 +0300

    ALL-00: global styles

---
+++ rename.sh
@@ -0,0 +1,28 @@
+sed -i -e 's/Default,Титосэ/Default-Pers-Chitose,Титосэ/g' *.ass
+sed -i -e 's/Default,Яэ/Default-Pers-Yae,Яэ/g' *.ass
+sed -i -e 's/Default,Кадзуха/Default-Pers-Kazuha,Кадзуха/g' *.ass
+sed -i -e 's/Default,Момока/Default-Pers-Momoka,Момока/g' *.ass
+sed -i -e 's/Default,Кото/Default-Pers-Koto,Кото/g' *.ass
+sed -i -e 's/Default,Нанами/Default-Pers-Nanami,Нанами/g' *.ass
+sed -i -e 's/Default,Годзё/Default-Pers-Gojo,Годзё/g' *.ass
+sed -i -e 's/Default,Муцуми/Default-Pers-Mutsumi,Муцуми/g' *.ass
+sed -i -e 's/Default,Менеджер Кадзу/Default-Pers-Kazuhas-Manager,Менеджер Кадзу/g' *.ass
+sed -i -e 's/Default,Мацуока/Default-Pers-Matsuoka,Мацуока/g' *.ass
+sed -i -e 's/Default,Кудзу/Default-Pers-Kuzu,Кудзу/g' *.ass
+sed -i -e 's/Default,Товада/Default-Pers-Towada,Товада/g' *.ass
+sed -i -e 's/Default,Президент/Default-Pers-President,Президент/g' *.ass
+sed -i -e 's/Default,Режиссёр/Default-Pers-Director,Режиссёр/g' *.ass
+sed -i -e 's/Default,Издатель/Default-Pers-IOC,Издатель/g' *.ass
+sed -i -e 's/Default,Линейный Продюсер/Default-Pers-Line-Producer,Линейный Продюсер/g' *.ass
+sed -i -e 's/Default,Звуковик/Default-Pers-Sound-Director,Звуковик/g' *.ass
+sed -i -e 's/Default,Персонал/Default-Pers-Staff,Персонал/g' *.ass
+sed -i -e 's/Default,Редактор/Default-Pers-Editor,Редактор/g' *.ass
+sed -i -e 's/Default,Автор/Default-Pers-Author,Автор/g' *.ass
+sed -i -e 's/Default,Сякудзии/Default-Pers-Syakujii,Сякудзии/g' *.ass
+sed -i -e 's/Default,Май/Default-Pers-MaiMai,Май/g' *.ass
+sed -i -e 's/Default,Сакура/Default-Pers-Sakura,Сакура/g' *.ass
+sed -i -e 's/Default,Папа Момоки/Default-Pers-Momokas-Father,Папа Момоки/g' *.ass
+sed -i -e 's/Default,Мама Кадзу/Default-Pers-Kazuhas-Mother,Мама Кадзу/g' *.ass
+sed -i -e 's/Default,Папа Кадзу/Default-Pers-Kazuhas-Father,Папа Кадзу/g' *.ass
+sed -i -e 's/Default,Ребёнок/Default-Pers-Kodomo,Ребёнок/g' *.ass
+sed -i -e 's/Default,ТВ/Default-TV,ТВ/g' *.ass
